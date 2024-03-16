3497deb66c7478479f615186972eb334774025e4
shehriar <shehriar1burney@gmail.com>
2024-03-16 05:42:26 +0500

    first commit

---
+++ bird.py
@@ -0,0 +1,39 @@
+import pygame
+JUMP_HEIGHT = 20
+Y_GRAVITY = 1
+
+class Bird:
+    def __init__(self):
+        self.height = 40
+        self.width = 40
+        self.color = (255, 255, 0)
+        self.xPos = 150
+        self.yPos = 400
+        self.isFlying = False
+        self.initYPos = self.yPos
+
+    def draw(self, screen):
+        if self.isFlying:
+            img = pygame.image.load("assets/bird-flying.png")
+        else:
+            img = pygame.image.load("assets/bird-falling.png")
+
+        birdPosition = (self.xPos, self.yPos)
+        img = pygame.transform.scale(img, (self.width, self.height))
+
+        screen.blit(img, birdPosition)
+        # pygame.draw.rect(screen, self.color, pygame.Rect(self.xPos, self.yPos, self.width, self.height))
+
+    def birdFalling(self):
+        FALLING_SPEED = 4
+        if self.yPos < 540:
+            self.yPos += FALLING_SPEED
+
+    def setInitYPos(self, yPos):
+        self.initYPos = yPos
+
+    def birdFlying(self):
+        if self.isFlying:
+            self.yPos -= 7
+            if self.yPos < self.initYPos - 60:
+                self.isFlying = False
---
+++ pipes.py
@@ -0,0 +1,29 @@
+import random
+import pygame
+
+class Pipes:
+    def __init__(self):
+        self.height = random.randint(50, 350)
+        self.width = 50
+        self.gap = 170
+        self.spaceBetweenPipes = 300
+        self.color = (0, 255, 0)
+        self.xPos = 500
+        self.yPos = 0
+
+
+    def drawPipes(self, screen):
+        topImg = pygame.image.load('assets/top-pipe.png')
+        bottomImg = pygame.image.load('assets/bottom-pipe.png')
+
+        topPipePosition = (self.xPos, self.yPos)
+        bottomPipePosition = (self.xPos, self.yPos + self.height + self.gap)
+
+        topImg = pygame.transform.scale(topImg, (self.width, self.height))
+        bottomImg = pygame.transform.scale(bottomImg, (self.width, 700 - self.yPos - self.height - self.gap))
+
+        screen.blit(topImg, topPipePosition)
+        screen.blit(bottomImg, bottomPipePosition)
+
+    def moving(self):
+        self.xPos -= 2
